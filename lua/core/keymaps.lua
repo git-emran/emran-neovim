f0c2f19f293f27b2eaba9849aed3ec71a01aac82
Emran Hossain <151853372+git-emran@users.noreply.github.com>
2025-08-12 23:20:01 +0600

    Code refactor

--- lua/core/keymaps.lua
+++ lua/core/keymaps.lua
@@ -84,3 +84,6 @@ vim.keymap.set("n", "<A-j>", ":m .+1<CR>==") -- Alt+j: Move line down
 vim.keymap.set("n", "<A-k>", ":m .-2<CR>==") -- Alt+k: Move line up
 vim.keymap.set("v", "<A-j>", ":m '>+1<CR>gv=gv") -- Alt+j: Move selection down
 vim.keymap.set("v", "<A-k>", ":m '<-2<CR>gv=gv") -- Alt+k: Move selection up
+
+-- Selecting all items
+vim.keymap.set("n", "<C-a>", "gg<S-v>G")
