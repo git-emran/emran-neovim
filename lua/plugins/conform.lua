e46be63c97d0458bdc0123d460e126058812c224
Emran Hossain <151853372+git-emran@users.noreply.github.com>
2025-10-23 15:48:57 +0600

    Refactor

--- lua/plugins/conform.lua
+++ lua/plugins/conform.lua
@@ -1,34 +1,24 @@
 return {
 	"stevearc/conform.nvim",
-	event = { "bufreadpre", "bufnewfile" },
+	event = { "BufReadPost", "BufNewFile" },
 	config = function()
 		require("conform").setup({
 			formatters_by_ft = {
-				c = { name = "clangd", timeout_ms = 500, lsp_format = "prefer" },
+				c = { "clang_format" },
 				lua = { "stylua" },
-				javascript = { "prettier", name = "dprint", timeout_ms = 500, lsp_format = "fallback" },
-				javascriptreact = { "prettier", name = "dprint", timeout_ms = 500, lsp_format = "fallback" },
-				typescript = { "prettier", name = "dprint", timeout_ms = 500, lsp_format = "fallback" },
-				typescriptreact = { "prettier", name = "dprint", timeout_ms = 500, lsp_format = "fallback" },
-				rust = { name = "rust_analyzer", timeout_ms = 500, lsp_format = "prefer" },
+				javascript = { "prettier" },
+				typescript = { "prettier" },
 				html = { "prettier" },
-				python = function(bufnr)
-					if require("conform").get_formatter_info("ruff_format", bufnr).available then
-						return { "ruff_format" }
-					else
-						return { "isort", "black" }
-					end
-				end,
 				css = { "prettier" },
 				json = { "prettier" },
 				yaml = { "prettier" },
 				markdown = { "prettier" },
-				go = { name = "gopls", timeout_ms = 500, lsp_format = "prefer" },
+				python = { "ruff_format" },
+				go = { "goimports", "gofmt" },
 				sh = { "shfmt" },
-				-- add more filetypes here
 			},
 			format_on_save = {
-				timeout_ms = 500, -- Increased timeout for robustness
+				timeout_ms = 500,
 				lsp_fallback = true,
 			},
 		})
