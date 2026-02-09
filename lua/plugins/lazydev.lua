74aeb8318e2c19ea102df0ba9ba91b2f6e89a203
Emran Hossain <151853372+git-emran@users.noreply.github.com>
2026-02-09 18:04:23 +0600

    removing rust implementation of blink completely

--- lua/plugins/lazydev.lua
+++ lua/plugins/lazydev.lua
@@ -1,84 +1,42 @@
 return {
-	-- LazyDev plugin for Lua completions
 	{
 		"folke/lazydev.nvim",
-		ft = "lua", -- load only for Lua files
+		ft = "lua", -- only load on lua files
 		opts = {
 			library = {
-				-- Load luvit types when `vim.uv` is detected
+				-- See the configuration section for more details
+				-- Load luvit types when the `vim.uv` word is found
 				{ path = "${3rd}/luv/library", words = { "vim%.uv" } },
 			},
 		},
 	},
-
-	-- Blink completion source for LazyDev, Lua-only implementation
-	{
+	{ -- optional cmp completion source for require statements and module annotations
+		"hrsh7th/nvim-cmp",
+		opts = function(_, opts)
+			opts.sources = opts.sources or {}
+			table.insert(opts.sources, {
+				name = "lazydev",
+				group_index = 0, -- set group index to 0 to skip loading LuaLS completions
+			})
+		end,
+	},
+	{ -- optional blink completion source for require statements and module annotations
 		"saghen/blink.cmp",
-		-- optional: provides snippets for the snippet source
-		dependencies = {
-			{ "rafamadriz/friendly-snippets" },
-		},
-
-		-- use a release tag to download pre-built binaries
-		version = "1.*",
-		-- AND/OR build from source, requires nightly: https://rust-lang.github.io/rustup/concepts/channels.html#working-with-nightly-rust
-		-- If you use nix, you can build from source using latest nightly rust with:
-		-- build = 'nix run .#build-plugin',
-
-		---@module 'blink.cmp'
 		opts = {
-			-- 'default' (recommended) for mappings similar to built-in completions (C-y to accept)
-			-- 'super-tab' for mappings similar to vscode (tab to accept)
-			-- 'enter' for enter to accept
-			-- 'none' for no mappings
-			--
-			-- All presets have the following mappings:
-			-- C-space: Open menu or open docs if already open
-			-- C-n/C-p or Up/Down: Select next/previous item
-			-- C-e: Hide menu
-			-- C-k: Toggle signature help (if signature.enabled = true)
-			--
-			-- See :h blink-cmp-config-keymap for defining your own keymap
-			keymap = { preset = "default" },
-
-			appearance = {
-				-- 'mono' (default) for 'Nerd Font Mono' or 'normal' for 'Nerd Font'
-				-- Adjusts spacing to ensure icons are aligned
-				nerd_font_variant = "mono",
-			},
-
-			-- (Default) Only show the documentation popup when manually triggered
-			completion = { documentation = { auto_show = false } },
-
-			-- Default list of enabled providers defined so that you can extend it
-			-- elsewhere in your config, without redefining it, due to `opts_extend`
-			sources = {
-				default = { "path" },
-			},
-
-			-- (Default) Rust fuzzy matcher for typo resistance and significantly better performance You may use a lua implementation instead by using `implementation = "lua"` or fallback to the lua implementation,
-			-- when the Rust fuzzy matcher is not available, by using `implementation = "prefer_rust"`
 			fuzzy = { implementation = "lua" },
-		},
-		opts_extend = { "sources.default" },
-	},
-	{
-		"j-hui/fidget.nvim",
-		tag = "v1.6.1",
-		lazy = false,
-		opts = {
-			progress = {
-				suppress_on_insert = true,
-				display = {
-					done_icon = "✔",
-					progress_icon = { pattern = "dots" },
-				},
-			},
-			notification = {
-				window = {
-					winblend = 0,
+			sources = {
+				-- add lazydev to your completion providers
+				default = { "lazydev", "path" },
+				providers = {
+					lazydev = {
+						name = "LazyDev",
+						module = "lazydev.integrations.blink",
+						-- make lazydev completions top priority (see `:h blink.cmp`)
+						score_offset = 100,
+					},
 				},
 			},
 		},
 	},
+	-- { "folke/neodev.nvim", enabled = false }, -- make sure to uninstall or disable neodev.nvim
 }
