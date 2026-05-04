50f0c917966ddc4bf83d8826a3a3b4d802cb15ff
Emran Hossain <151853372+git-emran@users.noreply.github.com>
2026-05-04 10:17:51 +0600

    adding opencode with qwen as a provider, Using totally free models for now

---
+++ lua/plugins/opencode.lua
@@ -0,0 +1,34 @@
+return {
+    'sudo-tee/opencode.nvim',
+    dependencies = {
+        'nvim-lua/plenary.nvim',
+        {
+            'meanderingprogrammer/render-markdown.nvim',
+            opts = {
+                anti_conceal = { enabled = false },
+                file_types = { 'markdown', 'opencode_output' },
+            },
+            ft = { 'markdown', 'avante', 'copilot-chat', 'opencode_output' },
+        },
+        -- optional, for file mentions and commands completion, pick only one
+        'saghen/blink.cmp',
+        -- 'hrsh7th/nvim-cmp',
+
+        -- optional, for file mentions picker, pick only one
+        -- 'folke/snacks.nvim',
+        'nvim-telescope/telescope.nvim',
+        -- 'ibhagwan/fzf-lua',
+        -- 'nvim_mini/mini.nvim',
+    },
+
+    config = function()
+        require('opencode').setup {
+            preferred_picker = 'telescope',
+            preferred_completion = 'blink',
+            default_mode = 'plan',
+            display_cost = true,
+            display_model = true,
+            default_model = 'openrouter/qwen/qwen3-coder-flash',
+        }
+    end,
+}
