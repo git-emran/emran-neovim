a14c8fd50cf24d2010aaea3b85bd7b9bedc8665c
Emran Hossain <151853372+git-emran@users.noreply.github.com>
2026-05-08 12:01:28 +0600

    removing neocodeium for now and plugin bump

--- lua/plugins/coding.lua
+++ lua/plugins/coding.lua
@@ -1,109 +1,105 @@
 return {
-	-- Surrounds code/word in parenthesis
-	{
-		"kylechui/nvim-surround",
-		version = "^3.0.0",
-		event = "VeryLazy",
-		config = function()
-			require("nvim-surround").setup({
-				keymaps = {
-					visual = "<leader>s",
-					visual_line = "<leader>S",
-				},
-			})
-		end,
-	},
+    -- Surrounds code/word in parenthesis
+    {
+        'kylechui/nvim-surround',
+        version = '^3.0.0',
+        event = 'VeryLazy',
+        config = function()
+            require('nvim-surround').setup {
+                keymaps = {
+                    visual = '<leader>s',
+                    visual_line = '<leader>S',
+                },
+            }
+        end,
+    },
 
-	-- Git view
-	{
-		"dinhhuy258/git.nvim",
-		event = "BufReadPre",
-		opts = {
-			keymaps = {
-				blame = "<Leader>gb",
-				browse = "<Leader>go",
-			},
-		},
-	},
+    -- Git view
+    {
+        'dinhhuy258/git.nvim',
+        event = 'BufReadPre',
+        opts = {
+            keymaps = {
+                blame = '<Leader>gb',
+                browse = '<Leader>go',
+            },
+        },
+    },
 
-	-- Autopair tags and braces
-	{
-		"windwp/nvim-autopairs",
-		event = "InsertEnter",
-		config = function()
-			local npairs = require("nvim-autopairs")
-			local Rule = require("nvim-autopairs.rule")
-			local conds = require("nvim-autopairs.conds")
+    -- Autopair tags and braces
+    {
+        'windwp/nvim-autopairs',
+        event = 'InsertEnter',
+        config = function()
+            local npairs = require 'nvim-autopairs'
+            local Rule = require 'nvim-autopairs.rule'
+            local conds = require 'nvim-autopairs.conds'
 
-			npairs.setup()
+            npairs.setup()
 
-			-- Autoclosing angle-brackets (MariaSolOs style)
-			npairs.add_rule(
-				Rule("<", ">", {
-					"-html",
-					"-javascriptreact",
-					"-typescriptreact",
-				})
-					:with_pair(conds.before_regex("%a+:?:?$", 3))
-					:with_move(function(opts)
-						return opts.char == ">"
-					end)
-			)
-		end,
-	},
+            -- Autoclosing angle-brackets (MariaSolOs style)
+            npairs.add_rule(Rule('<', '>', {
+                '-html',
+                '-javascriptreact',
+                '-typescriptreact',
+            }):with_pair(conds.before_regex('%a+:?:?$', 3)):with_move(function(opts)
+                return opts.char == '>'
+            end))
+        end,
+    },
 
-	-- Code folding
-	{
-		"chrisgrieser/nvim-origami",
-		event = "VeryLazy",
-		opts = {
-			foldKeymaps = {
-				setup = false,
-				hOnlyOpensOnFirstColumn = false,
-			},
-			pauseFoldsOnSearch = true,
-			autoFold = {
-				enabled = false,
-			},
-			foldText = {
-				enabled = true,
-				padding = 3,
-				lineCount = {
-					template = "%d lines",
-					hlgroup = "Comment",
-				},
-			},
-		},
-		init = function()
-			vim.opt.foldlevel = 99
-			vim.opt.foldlevelstart = 99
-		end,
-	},
+    -- Code folding
+    {
+        'chrisgrieser/nvim-origami',
+        event = 'VeryLazy',
+        opts = {
+            foldKeymaps = {
+                setup = false,
+                hOnlyOpensOnFirstColumn = false,
+            },
+            pauseFoldsOnSearch = true,
+            autoFold = {
+                enabled = false,
+            },
+            foldText = {
+                enabled = true,
+                padding = 3,
+                lineCount = {
+                    template = '%d lines',
+                    hlgroup = 'Comment',
+                },
+            },
+        },
+        init = function()
+            vim.opt.foldlevel = 99
+            vim.opt.foldlevelstart = 99
+        end,
+    },
 
-	-- Auto <tag> completion
-	{
-		"windwp/nvim-ts-autotag",
-		event = "InsertEnter",
-		dependencies = {
-			"nvim-treesitter/nvim-treesitter",
-		},
-		opts = {
-			opts = {
-				enable_close = true,
-				enable_rename = false,
-				enable_close_on_slash = true,
-			},
-		},
-		config = function(_, opts)
-			require("nvim-ts-autotag").setup(opts)
-		end,
-	},
+    -- Auto <tag> completion
+    {
+        'windwp/nvim-ts-autotag',
+        event = 'InsertEnter',
+        dependencies = {
+            'nvim-treesitter/nvim-treesitter',
+        },
+        opts = {
+            opts = {
+                enable_close = true,
+                enable_rename = false,
+                enable_close_on_slash = true,
+            },
+        },
+        config = function(_, opts)
+            require('nvim-ts-autotag').setup(opts)
+        end,
+    },
 
-	-- Code commenting
-	{
-		"folke/ts-comments.nvim",
-		opts = {},
-		event = "VeryLazy",
-		enabled = vim.fn.has("nvim-0.10.0") == 1,
-	},
+    -- Code commenting
+    {
+        'folke/ts-comments.nvim',
+        opts = {},
+        event = 'VeryLazy',
+        enabled = vim.fn.has 'nvim-0.10.0' == 1,
+    },
 }
